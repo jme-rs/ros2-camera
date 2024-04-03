c440a59ef6c017a45abd69fb28c19de58d7c6ec0
jme-rs <jme.co.jp@gmail.com>
2024-04-04 01:41:41 +0900

    画像にポーズを描画し、転倒判定を行う機能を追加

--- display_node/src/display_node/display_node/display_node.py
+++ display_node/src/display_node/display_node/display_node.py
@@ -6,6 +6,12 @@ import cv2
 
 
 class DisplayNode(Node):
+    """
+    画像とポーズ情報を受信し、画像にポーズを描画するノード。
+    転倒判定も行う。
+    """
+
+    """OpenPose のキーポイントのペア"""
     POSE_PAIRS = [
         [1, 2],
         [1, 5],
@@ -28,32 +34,122 @@ class DisplayNode(Node):
         [5, 16],
     ]
 
-    def __init__(self, pose_topic="/pose", camera_topic="/camera"):
-        super().__init__("display_node")
+    def __init__(self, pose_topic="/pose", camera_topic="/camera", fall_threshold=0.5):
+        """
+        ## Parameters
 
+        - pose_topic: ポーズ情報を受信するトピック名
+        - camera_topic: カメラ画像を受信するトピック名
+        - fall_threshold: 転倒判定の閾値
+        """
+
+        super().__init__("display_node")
         self.create_subscription(String, pose_topic, self._sub_pose_callback, 10)
         self.create_subscription(Image, camera_topic, self._sub_camera_callback, 10)
+
+        """CvBridge インスタンス"""
         self._cv_bridge = CvBridge()
-        self._pose_buffer = []
+
+        """ポーズ情報をバッファリングするリスト"""
+        self._pose_buffer: list = None
+
+        """転倒判定の閾値"""
+        self._fall_threshold = fall_threshold
 
     def _sub_camera_callback(self, image: Image):
+        """
+        カメラ画像を受信したときのコールバック関数。
+        画像にポーズを描画し、表示する。
+        """
+
         self.get_logger().info("Received: /camera")
         cv_image = self._cv_bridge.imgmsg_to_cv2(image)
-        # for pair in self.POSE_PAIRS:
-        #     part_a = pair[0]
-        #     part_b = pair[1]
-        #     cv2.line(
-        #         cv_image,
-        #         (part_a[0], part_a[1]),
-        #         (part_b[0], part_b[1]),
-        #         (0, 255, 0),
-        #         2,
-        #     )
+
+        self._draw_pose(cv_image)
+        self._put_text(cv_image)
 
         cv2.imshow("Display Node", cv_image)
         cv2.waitKey(1)
 
     def _sub_pose_callback(self, pose: String):
+        """
+        ポーズ情報を受信したときのコールバック関数。
+        ポーズ情報をバッファリングする。
+        """
+
         self.get_logger().info("Received: /pose")
         print(pose.data)
+
+        # `/pose` トピックと `/camera` トピックの受信は非同期であるため、
+        # バッファリングすることで参照できるようにする
         self._pose_buffer = pose.data
+
+    def _draw_pose(self, image):
+        """画像にポーズを描画する。"""
+
+        # ポーズがバッファされていない場合は画像を変更せずに返す
+        if self._pose_buffer is None:
+            return
+
+        for pair in self.POSE_PAIRS:
+            part_a_index = pair[0]
+            part_b_index = pair[1]
+            part_a_pos = self._pose_buffer[part_a_index].split(",")
+            part_b_pos = self._pose_buffer[part_b_index].split(",")
+
+            part_a_pos = (int(part_a_pos[0]), int(part_a_pos[1]))
+            part_b_pos = (int(part_b_pos[0]), int(part_b_pos[1]))
+
+            # 座標が 0 に近い場合は描画しない
+            if (part_a_pos[0] == 0 and part_a_pos[1] == 0) or (
+                part_b_pos[0] == 0 and part_b_pos[1] == 0
+            ):
+                continue
+
+            cv2.line(
+                image,
+                part_a_pos,
+                part_b_pos,
+                (0, 255, 0),
+                2,
+            )
+
+    def _put_text(self, image):
+        """画像にテキストを描画する処理はここにまとめて記述する。"""
+
+        # 転倒判定の結果を描画
+        if self._is_fallen(image.shape[0], self._pose_buffer):
+            cv2.putText(
+                image,
+                "FALLEN",
+                (10, 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 0, 255),
+                2,
+                cv2.LINE_AA,
+            )
+
+    def _is_fallen(self, frame_height, keypoints):
+        """
+        転倒判定を行う。
+        閾値は画像上部からの割合で指定する。
+        複数人の場合は、一人でも転倒していれば True を返す。
+        """
+
+        if keypoints is None:
+            return False
+
+        threshold_height = self._fall_threshold * frame_height
+
+        for person in keypoints:
+            flag = True
+            for part in person:
+                if part[0] == 0.0 and part[1] == 0.0:
+                    continue
+                if part[1] < threshold_height:
+                    flag = False
+                    break
+            if flag:
+                return True
+        return False
